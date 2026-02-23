e15d2d60e46ce87c21911dc89a854d25eb39b8c5
daniel-prestwich <prestwich17@gmail.com>
2026-02-23 00:13:59 +0000

    fixed minor issues with judge tool

--- backend/app/services/judge_tools.py
+++ backend/app/services/judge_tools.py
@@ -1,19 +1,18 @@
-from pathlib import Path
+
 from datetime import datetime
 import os
 import json
 
 
-
 def save_evaluation_result(agent_name: str, score: float, summary: str) -> bool:
-    folder = "./evalutation.logs" #could be unsafe?
-    file_name = agent_name + datetime.now().isoformat + ".json"
+    folder = "./evaluation_logs" #could be unsafe?
+    file_name = agent_name + datetime.now().isoformat() + ".json"
     file_path = os.path.join(folder, file_name) 
     data = {
         "agent" : agent_name,
-        "agent score " : score,
-        "retrieved at: " : datetime.now().isoformat,
-        "Summary result" : summary
+        "agent_score " : score,
+        "retrieved_at: " : datetime.now().isoformat(),
+        "Summary_result" : summary
     }
     try:
         with open(file_path, "w") as f:
@@ -26,3 +25,4 @@ def save_evaluation_result(agent_name: str, score: float, summary: str) -> bool:
 
 
 
+ 
